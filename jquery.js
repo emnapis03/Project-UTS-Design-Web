90ff81f0ad62f9fd4a93b6ce7560bd7e26450a4f
emnapis03 <mohammadnafis602@gmail.com>
2023-05-15 10:29:07 +0700

    Penambahan JS

--- jquery.js
+++ jquery.js
@@ -1,3 +1,13 @@
 function out(){
     return alert("Apakah Anda ingin keluar?")
 }
+
+
+let popup = document.getElementById("popup");
+
+function openpopup(){
+    popup.classList.add("open-popup");
+}
+function closepopup(){
+    popup.classList.remove("open-popup");
+}
